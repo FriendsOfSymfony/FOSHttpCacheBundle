88d24c422d13eff1ceaf71ba991e777eeeead9d2
David de Boer <david@ddeboer.nl>
2013-12-26 14:20:34 +0100

    Don't parse URL if Host header is already set

--- Invalidation/Varnish.php
+++ Invalidation/Varnish.php
@@ -149,9 +149,15 @@ class Varnish implements BanInterface, PurgeInterface, RefreshInterface
     {
         $request = $this->client->createRequest($method, $url, $headers);
 
-        // If $url doesn't contain a hostname, and Host header hasn't yet been
-        // set, set the Host header to the default hostname
-        $parsedUrl = parse_url($url);
+        // If Host headers hasn't been set and $url doesn't contain a hostname,
+        // set the Host header to the default hostname
+        if ('' != $request->getHeader('Host')) {
+            $parsedUrl = parse_url($url);
+            if (!isset($parsedUrl['host'])) {
+                $request->setHeader('Host', $this->host);
+            }
+        }
+
         if (!isset($parsedUrl['host']) && '' != $request->getHeader('Host')
         ) {
             $request->setHeader('Host', $this->host);
