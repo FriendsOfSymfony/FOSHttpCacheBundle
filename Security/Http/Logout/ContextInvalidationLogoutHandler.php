1d4ded13e7366fe8f9e38379aaa02c2f43be6c41
Joel Wurtz <jwurtz@jolicode.com>
2014-06-30 14:35:50 +0200

    Add hash invalidation on logout

---
+++ Security/Http/Logout/ContextInvalidationLogoutHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the FOSHttpCacheBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\HttpCacheBundle\Security\Http\Logout;
+
+use FOS\HttpCache\ProxyClient\Invalidation\BanInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
+
+class ContextInvalidationLogoutHandler implements LogoutHandlerInterface
+{
+    /**
+     * Service used to ban hash request
+     *
+     * @var \FOS\HttpCache\ProxyClient\Invalidation\BanInterface
+     */
+    private $banner;
+
+    /**
+     * Accept header
+     *
+     * @var string
+     */
+    private $acceptHeader;
+
+    /**
+     * User identifier headers
+     *
+     * @var string[]
+     */
+    private $userIdentifierHeaders;
+
+    public function __construct(BanInterface $banner, $userIdentifierHeaders, $acceptHeader)
+    {
+        $this->banner                = $banner;
+        $this->acceptHeader          = $acceptHeader;
+        $this->userIdentifierHeaders = $userIdentifierHeaders;
+    }
+
+    /**
+     * Invalidate the user context hash
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param TokenInterface $token
+     */
+    public function logout(Request $request, Response $response, TokenInterface $token)
+    {
+        $sessionId = $request->getSession()->getId();
+
+        foreach ($this->userIdentifierHeaders as $header) {
+            $this->banner->ban(array(
+                'accept' => $this->acceptHeader,
+                $header => sprintf('.*%s.*', $sessionId)
+            ));
+        }
+    }
+}
--- Tests/Functional/Fixtures/app/AppKernel.php
+++ Tests/Functional/Fixtures/app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \FOS\HttpCacheBundle\FOSHttpCacheBundle(),
         );
     }
@@ -48,5 +49,18 @@ class AppKernel extends Kernel
     protected function getContainerBaseClass()
     {
         return '\PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function prepareContainer(\Symfony\Component\DependencyInjection\ContainerBuilder $container)
+    {
+        parent::prepareContainer($container);
+
+        $container->setDefinition(
+            'session.test_storage',
+            new \Symfony\Component\DependencyInjection\Definition('FOS\HttpCacheBundle\Tests\Functional\Fixtures\Session\TestSessionStorage')
+        );
     }
 }
