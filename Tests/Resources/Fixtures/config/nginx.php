1c988bffbe2a642a3a32d792632d7ed2d432a3ef
David Buchmann <david.buchmann@liip.ch>
2014-07-15 18:09:44 +0200

    allow to configure a custom guzzle client for cache proxy requests

--- Tests/Resources/Fixtures/config/nginx.php
+++ Tests/Resources/Fixtures/config/nginx.php
@@ -5,6 +5,7 @@ $container->loadFromExtension('fos_http_cache', array(
         'nginx' => array(
             'servers' => array('22.22.22.22'),
             'base_url' => '/test',
+            'guzzle_client' => 'acme.guzzle.nginx',
             'purge_location' => '/purge',
         ),
     ),
