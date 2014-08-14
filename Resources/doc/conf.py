68d07dcd93eec832373c0e90693b301e310d2258
David de Boer <david@ddeboer.nl>
2014-08-14 09:38:41 +0200

    Add Sphinx build to Travis and fix warnings

--- Resources/doc/conf.py
+++ Resources/doc/conf.py
@@ -62,7 +62,7 @@ release = '1.0.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'includes']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
