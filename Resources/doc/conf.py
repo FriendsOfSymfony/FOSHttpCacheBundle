44da5a7deaf43dabb0c240e9a3ee3b3ff8dddb8a
David de Boer <david@ddeboer.nl>
2015-05-27 14:19:56 +0200

    Don't use EN dash to fix doc build

--- Resources/doc/conf.py
+++ Resources/doc/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'FOSHttpCacheBundle'
-copyright = '2014–2015, David de Boer, David Buchmann'
+copyright = '2014-2015, David de Boer, David Buchmann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
