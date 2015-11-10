efab5ecb99bd7b7683e2569590c67d6a2fc726ed
Alex Wilson <a@ax.gy>
2015-11-10 00:45:47 +0000

    Enable prioritising tagged definitions, to allow for reliable arrangements.

--- Tests/Unit/DependencyInjection/Compiler/HashGeneratorPassTest.php
+++ Tests/Unit/DependencyInjection/Compiler/HashGeneratorPassTest.php
@@ -14,6 +14,8 @@ namespace FOS\HttpCacheBundle\Tests\Unit\DependencyInjection\Compiler;
 use FOS\HttpCacheBundle\DependencyInjection\Compiler\HashGeneratorPass;
 use FOS\HttpCacheBundle\DependencyInjection\FOSHttpCacheExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 class HashGeneratorPassTest extends \PHPUnit_Framework_TestCase
@@ -57,6 +59,46 @@ class HashGeneratorPassTest extends \PHPUnit_Framework_TestCase
         $config['user_context']['enabled'] = true;
         $this->extension->load(array($config), $container);
         $this->userContextListenerPass->process($container);
+    }
+
+    /**
+     * Test that priorities allow for us to re-arrange services.
+     */
+    public function testConfigPrioritisedProviders()
+    {
+        $container = $this->createContainer();
+
+        $definitions = array(
+
+            'test.provider' => (new Definition)
+                ->setClass('\\stdClass')
+                ->addTag(HashGeneratorPass::TAG_NAME, array('priority' => 10)),
+
+            'foo.provider' => (new Definition)
+                ->setClass('\\stdClass')
+                ->addTag(HashGeneratorPass::TAG_NAME),
+
+            'bar.provider' => (new Definition)
+                ->setClass('\\stdClass')
+                ->addTag(HashGeneratorPass::TAG_NAME, array('priority' => 5)),
+
+        );
+
+        $container->setDefinitions($definitions);
+
+        $config = $this->getBaseConfig();
+        $config['user_context']['enabled'] = true;
+        $this->extension->load(array($config), $container);
+        $this->userContextListenerPass->process($container);
+
+        $arguments = $container->getDefinition('fos_http_cache.user_context.hash_generator')->getArguments();
+        $services = array_map(
+            function($argument) {
+                return (string)$argument;
+            }, array_shift($arguments)
+        );
+
+        $this->assertEquals($services, array('test.provider', 'bar.provider', 'foo.provider'));
     }
 
     protected function createContainer()
