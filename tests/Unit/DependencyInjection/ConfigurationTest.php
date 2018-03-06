3477d63730b9a03f68a0c2a04328003badeb9ff3
David Buchmann <david.buchmann@liip.ch>
2018-03-06 09:50:03 +0100

    re-enable test now that matthiasnoback/symfony-config-test is fixed

--- tests/Unit/DependencyInjection/ConfigurationTest.php
+++ tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -30,9 +30,6 @@ class ConfigurationTest extends AbstractExtensionConfigurationTestCase
 
     public function testEmptyConfiguration()
     {
-        $this->markTestSkipped('We need to update to the latest SymfonyConfigTest version and see if the bug in AbstractConfigurationConstraint.php
- is still present');
-
         $expectedConfiguration = $this->getEmptyConfig();
 
         $formats = array_map(function ($path) {
