3345602511b4b421947a6319c0568a16a940a5b3
Alexander Schranz <alexander@sulu.io>
2018-10-23 19:01:14 +0200

    increase year in doc

--- Resources/doc/conf.py
+++ Resources/doc/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'FOSHttpCacheBundle'
-copyright = '2014-2017, David de Boer, David Buchmann'
+copyright = '2014-2018, David de Boer, David Buchmann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
