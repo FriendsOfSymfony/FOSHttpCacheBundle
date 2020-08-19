853eaa0ea8bdcbf44769fc935f680dd843d189e0
Alexander Schranz <alexander@sulu.io>
2020-08-19 16:37:38 +0200

    Throw Exception when logout handler is called

--- src/Security/Http/Logout/ContextInvalidationLogoutHandler.php
+++ src/Security/Http/Logout/ContextInvalidationLogoutHandler.php
@@ -15,6 +15,7 @@ use FOS\HttpCacheBundle\UserContextInvalidator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 
 /**
@@ -38,6 +39,13 @@ final class ContextInvalidationLogoutHandler implements LogoutHandlerInterface
     public function logout(Request $request, Response $response, TokenInterface $token)
     {
         @trigger_error('Using the ContextInvalidationLogoutHandler is deprecated', E_USER_DEPRECATED);
+
+        if (class_exists(LogoutEvent::class)) {
+            // This function should not longer be called in Symfony 5.1
+            // See also: https://github.com/FriendsOfSymfony/FOSHttpCacheBundle/pull/545#discussion_r465089219
+            throw new \LogicException(__CLASS__.'::'.__METHOD__.' is deprecated and should since Symfony 5.1 not longer be called.');
+        }
+
         $this->invalidator->invalidateContext($request->getSession()->getId());
     }
 }
