ce6f9c40b10022f8bd5bc016ac3e1bbaf3583285
Jeroen Thora <jeroenthora@gmail.com>
2022-04-10 20:00:22 +0200

    Use loginUser method of WebTestCase to use authenticated users (or polyfill for older symfony versions)

--- tests/Functional/EventListener/SwitchUserListenerTest.php
+++ tests/Functional/EventListener/SwitchUserListenerTest.php
@@ -12,18 +12,17 @@
 namespace FOS\HttpCacheBundle\Tests\Functional\EventListener;
 
 use FOS\HttpCache\ProxyClient\Varnish;
-use FOS\HttpCacheBundle\Tests\Functional\SessionHelperTrait;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\User\InMemoryUser;
 use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 if (!\class_exists(KernelBrowser::class)) {
     \class_alias(Client::class, KernelBrowser::class);
@@ -32,18 +31,15 @@ if (!\class_exists(KernelBrowser::class)) {
 class SwitchUserListenerTest extends WebTestCase
 {
     use MockeryPHPUnitIntegration;
-    use SessionHelperTrait;
 
     private const FIREWALL_NAME = 'secured_area';
-    private const SESSION_ID = 'test';
     private $sessionName;
     private static $overrideService = false;
 
     public function testSwitchUserCompatibility()
     {
         $client = static::createClient();
-        $this->callInRequestContext($client, [$this, 'loginAsAdmin']);
-        $client->getCookieJar()->set(new Cookie($this->sessionName, self::SESSION_ID));
+        $this->loginTestUser($client);
 
         $client->request('GET', '/secured_area/switch_user?_switch_user=user');
         $client->request('GET', '/secured_area/switch_user');
@@ -61,37 +57,21 @@ class SwitchUserListenerTest extends WebTestCase
         $mock = \Mockery::mock(Varnish::class);
         $mock->shouldReceive('invalidateTags')
             ->once()
-            ->with(['fos_http_cache_hashlookup-test']);
+            ->with(\Mockery::any())
+        ;
 
         $mock->shouldReceive('flush')
             ->once()
             ->andReturn(1);
 
-        $kernel = static::createKernel();
-        $kernel->boot();
-        $kernel->getContainer()->set('fos_http_cache.proxy_client.varnish', $mock);
-
         $client = static::createClient();
 
-        $this->callInRequestContext($client, [$this, 'loginAsAdmin']);
+        $container = method_exists($this, 'getContainer') ? self::getContainer() : $client->getContainer();
+        $container->set('fos_http_cache.proxy_client.varnish', $mock);
+
+        $this->loginTestUser($client);
 
         $client->request('GET', '/secured_area/switch_user?_switch_user=user');
-    }
-
-    public function loginAsAdmin(RequestEvent $requestEvent)
-    {
-        $session = $requestEvent->getRequest()->getSession();
-        if (Kernel::MAJOR_VERSION >= 6) {
-            $token = new UsernamePasswordToken(new InMemoryUser('admin', 'admin', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']), self::FIREWALL_NAME, ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
-        } else {
-            $token = new UsernamePasswordToken(new User('admin', 'admin', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']), null, self::FIREWALL_NAME, ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
-        }
-
-        $session->setId(self::SESSION_ID);
-        $session->set(sprintf('_security_%s', self::FIREWALL_NAME), serialize($token));
-        $session->save();
-
-        $this->sessionName = $session->getName();
     }
 
     protected static function createKernel(array $options = []): KernelInterface
@@ -103,5 +83,37 @@ class SwitchUserListenerTest extends WebTestCase
         }
 
         return $kernel;
+    }
+
+    private function getUser(): UserInterface
+    {
+        if (Kernel::MAJOR_VERSION >= 6) {
+            $testUser = new InMemoryUser('admin', 'admin', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
+        } else {
+            $testUser = new User('admin', 'admin', ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH']);
+        }
+
+        return $testUser;
+    }
+
+    private function loginTestUser(KernelBrowser $client)
+    {
+        if (method_exists($client, 'loginUser')) {
+            $client->loginUser($this->getUser(), self::FIREWALL_NAME);
+
+            return;
+        }
+
+        $container = method_exists($this, 'getContainer') ? self::getContainer() : (property_exists($this, 'container') ? self::$container : $client->getContainer());
+        $session = $container->get('session');
+
+        $user = $this->getUser();
+
+        $token = new UsernamePasswordToken($user, null, self::FIREWALL_NAME, $user->getRoles());
+        $session->set('_security_'.self::FIREWALL_NAME, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
     }
 }
