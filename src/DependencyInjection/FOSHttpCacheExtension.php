e9d39bfecbcb665c3744ea138826fb8df132fdcf
Jacob Dreesen <j.dreesen@neusta.de>
2025-06-16 17:11:29 +0200

    Set ProxyClient class alias to default proxy client
    When only cache_manager.custom_proxy_client is configured.

--- src/DependencyInjection/FOSHttpCacheExtension.php
+++ src/DependencyInjection/FOSHttpCacheExtension.php
@@ -82,6 +82,10 @@ class FOSHttpCacheExtension extends Extension
                     'fos_http_cache.default_proxy_client',
                     $config['cache_manager']['custom_proxy_client']
                 );
+                $container->setAlias(
+                    ProxyClient::class,
+                    'fos_http_cache.default_proxy_client'
+                );
             }
             if ('auto' === $config['cache_manager']['generate_url_type']) {
                 if (array_key_exists('custom_proxy_client', $config['cache_manager'])) {
